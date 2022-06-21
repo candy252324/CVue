40997d153fc880f27aac2f0c411f60f6670b03b0
cjh <1964646121@qq.com>
2022-06-21 15:28:56 +0800

    watcher 去重

--- src/Dep.mjs
+++ src/Dep.mjs
@@ -3,6 +3,8 @@ export default class Dep {
     this.watchers = []
   }
   depend() {
+    // 防止 watcher 被重复收集
+    if (this.watchers.includes(Dep.target)) return
     this.watchers.push(Dep.target)
     console.log("depend:watchers!!!!!!")
     console.log(this.watchers)
